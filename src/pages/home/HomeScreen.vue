d26c9714f3d0998506f2db460acbc93a629faa6d
Pham Hong <phamhong@Mac-mini-cua-Pham.local>
2025-09-10 10:55:05 +0700

    fix

--- src/pages/home/HomeScreen.vue
+++ src/pages/home/HomeScreen.vue
@@ -4,6 +4,23 @@ export default {
     goToAbout() {
       this.$router.push('/about')
     },
+    async handleConfigUIApp() {
+      const { configUIApp } = await import('x-app-sdk');
+      
+
+
+      const config = {
+        headerTitle: 'Tên ứng dụng',
+        headerTextColor: '#ffffff',
+        headerColor: '#000000'
+      };
+      try {
+        const response = await configUIApp(config);
+        console.log('Cấu hình thành công:', response);
+      } catch (error) {
+        console.error('Lỗi cấu hình:', error);
+      }
+    },
   },
 }
 </script>
@@ -11,4 +28,5 @@ export default {
 <template>
   <h2>HomeScreen</h2>
   <button @click="goToAbout">Go to About</button>
+    <button @click="handleConfigUIApp">Cấu hình UI App</button>
 </template>
